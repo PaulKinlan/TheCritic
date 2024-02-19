b1ae1597f76ec302de0d6028d98dbe66879c1000
PaulKinlan <paul.kinlan@gmail.com>
2024-02-19 20:01:44 +0000

    Removing dist.

--- dist/server/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { ProxyServerConfig } from "@google-labs/breadboard/remote";
-export declare const startServer: (dist: string, port: string, config: ProxyServerConfig) => Promise<void>;
-//# sourceMappingURL=index.d.ts.map
